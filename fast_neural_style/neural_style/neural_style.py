1d064de419443bbd97b51c3ad47e6780dd3005c8
Ofek <ofek.cohen1@gmail.com>
2019-06-06 10:39:02 +0300

    Changed neural_style.py to match 2 pictures and better variable names

--- fast_neural_style/neural_style/neural_style.py
+++ fast_neural_style/neural_style/neural_style.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision import transforms
 import torch.onnx
-import fast_neural_style.neural_style.MyDataSet as MyDataSet
+from fast_neural_style.neural_style.MyDataSet import MyDataSet
 
 import fast_neural_style.neural_style.utils as utils
 from fast_neural_style.neural_style.transformer_net import TransformerNet
@@ -30,8 +30,9 @@ def check_paths(args):
 
 
 def train(dataset_path, style_image_path, save_model_dir, has_cuda,
-                 epochs=2, batch_size=4, checkpoint_model_dir=None, image_size=256, style_size=None, seed=42, content_weight=1e5,
-                 style_weight=1e10, lr=1e-3, log_interval=500, checkpoint_interval=2000):
+          epochs=2, batch_size=4, checkpoint_model_dir=None, image_size=256, style_size=None, seed=42,
+          content_weight=1e5,
+          style_weight=1e10, lr=1e-3, log_interval=500, checkpoint_interval=2000):
     device = torch.device("cuda" if has_cuda else "cpu")
 
     np.random.seed(seed)
@@ -41,20 +42,24 @@ def train(dataset_path, style_image_path, save_model_dir, has_cuda,
         transforms.Resize(image_size),
         transforms.CenterCrop(image_size),
         transforms.ToTensor(),
-        transforms.Lambda(lambda x: x.mul(255))
+        transforms.Lambda(lambda x: x.mul(255))  # TODO: Change Normalization to ImageNet Norm
     ])
-    # train_dataset = datasets.ImageFolder(dataset_path, transform)
-    train_dataset = MyDataSet(dataset_path, transform)
-    train_loader = DataLoader(train_dataset, batch_size=batch_size)
+    videos_list = os.listdir(dataset_path)
+    train_dataset = {}
+    train_loader = {}
+    for video_name in videos_list:
+        video_dataset_path = os.path.join(dataset_path, video_name)
+        train_dataset[video_name] = MyDataSet(video_dataset_path, transform)
+        train_loader = DataLoader(train_dataset[video_name], batch_size=batch_size)
 
-    transformer = TransformerNet().to(device)
-    optimizer = Adam(transformer.parameters(), lr)
+    transformer_net = TransformerNet().to(device)
+    optimizer = Adam(transformer_net.parameters(), lr)
     mse_loss = torch.nn.MSELoss()
 
     vgg = Vgg16(requires_grad=False).to(device)
     style_transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Lambda(lambda x: x.mul(255))
+        transforms.Lambda(lambda x: x.mul(255))  # TODO: Change Normalization to ImageNet Norm
     ])
     style = utils.load_image(style_image_path, size=style_size)
     style = style_transform(style)
@@ -64,66 +69,69 @@ def train(dataset_path, style_image_path, save_model_dir, has_cuda,
     gram_style = [utils.gram_matrix(y) for y in features_style]
 
     for e in range(epochs):
-        transformer.train()
+        transformer_net.train()
         agg_content_loss = 0.
         agg_style_loss = 0.
         count = 0
-        for batch_id, (x, _) in enumerate(train_loader):
-            n_batch = len(x)
-            count += n_batch
-            optimizer.zero_grad()
+        for video_name in videos_list:
+            for batch_num, both_frames in enumerate(train_loader[video_name]):
+                for frame in both_frames:
+                    bath_size = len(frame)
+                    count += bath_size
+                    optimizer.zero_grad()
 
-            x = x.to(device)
-            y = transformer(x)
+                    frame = frame.to(device)
+                    frame_style = transformer_net(frame)
 
-            y = utils.normalize_batch(y)
-            x = utils.normalize_batch(x)
+                    frame_style = utils.normalize_batch(frame_style)
+                    frame = utils.normalize_batch(frame)
 
-            features_y = vgg(y)
-            features_x = vgg(x)
+                    features_frame = vgg(frame)
+                    features_frame_style = vgg(frame_style)
 
-            content_loss = content_weight * mse_loss(features_y.relu2_2, features_x.relu2_2)
+                    content_loss = content_weight * mse_loss(features_frame.relu2_2, features_frame_style.relu2_2)
 
-            style_loss = 0.
-            for ft_y, gm_s in zip(features_y, gram_style):
-                gm_y = utils.gram_matrix(ft_y)
-                style_loss += mse_loss(gm_y, gm_s[:n_batch, :, :])
-            style_loss *= style_weight
+                    style_loss = 0.
+                    for ft_frame_style, gm_s in zip(features_frame_style, gram_style):  # loop on feature layers
+                        gm_frame_style = utils.gram_matrix(ft_frame_style)
+                        style_loss += mse_loss(gm_frame_style, gm_s[:bath_size, :, :])
+                    style_loss *= style_weight
 
-            total_loss = content_loss + style_loss
-            total_loss.backward()
-            optimizer.step()
+                    total_loss = content_loss + style_loss
+                    total_loss.backward()
+                    optimizer.step()
 
-            agg_content_loss += content_loss.item()
-            agg_style_loss += style_loss.item()
+                    agg_content_loss += content_loss.item()
+                    agg_style_loss += style_loss.item()
 
-            if (batch_id + 1) % log_interval == 0:
-                mesg = "{}\tEpoch {}:\t[{}/{}]\tcontent: {:.6f}\tstyle: {:.6f}\ttotal: {:.6f}".format(
-                    time.ctime(), e + 1, count, len(train_dataset),
-                                  agg_content_loss / (batch_id + 1),
-                                  agg_style_loss / (batch_id + 1),
-                                  (agg_content_loss + agg_style_loss) / (batch_id + 1)
-                )
-                print(mesg)
+                    if (batch_num + 1) % log_interval == 0:
+                        mesg = "{}\tEpoch {}:\t[{}/{}]\tcontent: {:.6f}\tstyle: {:.6f}\ttotal: {:.6f}".format(
+                            time.ctime(), e + 1, count, 2 * len(train_dataset),
+                            content_loss.item(),
+                            style_loss.item(),
+                            (content_loss.item() + style_loss.item())
+                        )
+                        print(mesg)
 
-            if checkpoint_model_dir is not None and (batch_id + 1) % checkpoint_interval == 0:
-                transformer.eval().cpu()
-                ckpt_model_filename = "ckpt_epoch_" + str(e) + "_batch_id_" + str(batch_id + 1) + ".pth"
-                ckpt_model_path = os.path.join(checkpoint_model_dir, ckpt_model_filename)
-                torch.save(transformer.state_dict(), ckpt_model_path)
-                transformer.to(device).train()
+                    if checkpoint_model_dir is not None and (batch_num + 1) % checkpoint_interval == 0:
+                        transformer_net.eval().cpu()
+                        ckpt_model_filename = "ckpt_epoch_" + str(e) + "_batch_id_" + str(batch_num + 1) + ".pth"
+                        ckpt_model_path = os.path.join(checkpoint_model_dir, ckpt_model_filename)
+                        torch.save(transformer_net.state_dict(), ckpt_model_path)
+                        transformer_net.to(device).train()
 
     # save model
-    transformer.eval().cpu()
-    save_model_filename = "epoch_" + str(epochs) + "_" + str(time.ctime()).replace(' ', '_') + "_" + str(
-        content_weight) + "_" + str(style_weight) + ".model"
+    transformer_net.eval().cpu()
+    # save_model_filename = "epoch_" + str(epochs) + "_" + str(time.ctime()).replace(' ', '_') + "_" + str(
+    #     content_weight) + "_" + str(style_weight) + ".model"
+    save_model_filename = "myModel.pth"
     save_model_path = os.path.join(save_model_dir, save_model_filename)
-    torch.save(transformer.state_dict(), save_model_path)
+    torch.save(transformer_net.state_dict(), save_model_path)
 
     print("\nDone, trained model saved at", save_model_path)
 
 
-def stylize(has_cuda, content_image, model, output_image_path = None, content_scale=None):
+def stylize(has_cuda, content_image, model, output_image_path=None, content_scale=None):
     device = torch.device("cuda" if has_cuda else "cpu")
 
     # content_image = utils.load_image(content_image_path, scale=content_scale)
@@ -155,6 +163,7 @@ def main():
     output_image_path = "../images/output-images/ofek_garden-test.jpg"
     model = "../models/mosaic.pth"
     stylize(has_cuda, content_image_path, output_image_path, model)
+
 
 if __name__ == "__main__":
     main()
--- fast_neural_style/test.py
+++ fast_neural_style/test.py
@@ -2,9 +2,63 @@ import torch
 from torchvision import models
 from fast_neural_style.neural_style.vgg import Vgg16
 from PIL import Image
-
+from fast_neural_style.neural_style.neural_style import train, stylize
+import os
+from torchvision import transforms
+import torch
 # model_vgg = Vgg16()
+from fast_neural_style.neural_style.MyDataSet import MyDataSet
+from torch.utils.data import DataLoader
+import numpy as np
 
 im = Image.open("images/content-images/amber.jpg")
-print(type(im))
-Image._show(im)
+# print(type(im))
+# im.show()
+
+
+image_size = 256
+
+transform = transforms.Compose([
+    transforms.Resize(image_size),
+    transforms.CenterCrop(image_size),
+    transforms.ToTensor(),
+    transforms.Lambda(lambda x: x.mul(255))
+])
+#
+dataset_path = "../Data"
+
+videos_list = os.listdir(dataset_path)
+train_dataset = {}
+train_loader = {}
+for video_name in videos_list:
+    video_dataset_path = os.path.join(dataset_path, video_name)
+    train_dataset[video_name] = MyDataSet(video_dataset_path, transform)
+    train_loader[video_name] = DataLoader(train_dataset[video_name], batch_size= 1)
+
+counter = 0
+for video_name in videos_list:
+    for batch_id, (frame_left,frame_right) in enumerate(train_loader[video_name]):
+        # frame_left, frame_right = data
+
+        frame_left = frame_left.clone().clamp(0, 255).numpy()
+        frame_left = frame_left.transpose(2, 3, 1, 0).astype("uint8")
+        frame_left = frame_left[:, :, :, 0]
+        frame_left = Image.fromarray(frame_left)
+        counter += 1
+    # frame_left.show()
+print(counter)
+# train(dataset_path, style_image_path, model_dir, 1, batch_size=1, log_interval=1)
+
+# model = "models/myModel.pth"
+# has_cuda = 1
+#
+# left_frame_stylized = stylize(has_cuda, im, model)
+# stylized_frame = left_frame_stylized.clone().clamp(0, 255).numpy()
+# stylized_frame = stylized_frame.transpose(1, 2, 0).astype("uint8")
+# img = Image.fromarray(stylized_frame)
+# img.show()
+
+# style_image_path = "../fast_neural_style/images/style-images/mosaic.jpg"
+# model_dir = "../fast_neural_style/models/"
+# videos_list = os.listdir("../Data")
+# print(type(videos_list))
